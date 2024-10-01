776a563afb9a602d4012eb5a36e75bdf5afd4ce0
Ethan Dickson <ethan@coder.com>
2024-10-01 11:21:44 +0000

    add agent and workspace insert notifs

---
+++ coderd/database/migrations/000260_workspace_updates.down.sql
@@ -0,0 +1,7 @@
+DROP TYPE agent_id_name_pair;
+
+DROP TRIGGER IF EXISTS new_workspace_notify ON workspaces;
+DROP FUNCTION IF EXISTS new_workspace_notify;
+
+DROP TRIGGER IF EXISTS new_agent_notify ON workspace_agents;
+DROP FUNCTION new_agent_notify;
